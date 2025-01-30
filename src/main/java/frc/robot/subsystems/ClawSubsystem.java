fc50a696b3c0ebe3f5d4f852e5988bd329f5c22f
LyraLz <lyranielsen83@wcpsstudent.org>
2025-01-29 19:43:27 -0500

    ClawSubsystem V1.0
    Claw subsystem initial write.

---
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -0,0 +1,119 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.hal.simulation.DIODataJNI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+import frc.robot.motor.Motors;
+import frc.robot.motor.Motor.encoderType;
+
+public class ClawSubsystem extends SubsystemBase {
+
+    private final Motors wristMotor;
+    private final Motors clawMotor;
+
+    private double desiredAngle;
+    private boolean rotationActive;
+
+    
+    public ClawSubsystem() {
+
+        wristMotor = new Motors(Constants.CLAW_PIVOT_MOTOR, encoderType.Absolute, false);
+        clawMotor = new Motors(Constants.CLAW_MOTOR);
+
+    }
+
+    // Wrist section
+
+
+    public void SetDesiredAngle(double angle) {
+
+        desiredAngle = angle;
+
+    }
+
+    private double GetCurrentAngle() {
+
+        return wristMotor.motor.getAbsoluteAngle();
+
+    }
+
+    private boolean IsInDeadzone(double deadzone) {
+
+        return desiredAngle > GetCurrentAngle() - deadzone && desiredAngle < GetCurrentAngle() + deadzone;
+
+    }
+
+    private boolean IsReady(double deadzone) {
+
+        return IsInDeadzone(deadzone) && wristMotor.GetVelocity() < 100;
+
+    }
+
+    private void ActivateRotation() {
+
+        rotationActive = true;
+
+    }
+
+    private void WristRotateUp(double speed) {
+
+        wristMotor.Spin(Math.abs(speed));
+
+    }
+
+    private void WristRotateDown(double speed) {
+
+        wristMotor.Spin(-Math.abs(speed));
+
+    }
+
+    private void WristStop() {
+
+        wristMotor.Spin(0);
+        rotationActive = false;
+
+    }
+
+    public void PivotToTarget(double speed) {
+
+        if (!rotationActive) return;
+
+        double angle = GetCurrentAngle();
+        double minAngle = 5;
+        double maxAngle = 175;
+
+        if (angle < minAngle || angle > maxAngle) {
+            WristStop();
+            return;
+        }
+
+        WristRotateUp(speed);
+
+    }
+
+    // Claw section
+    public double GetClawPosition() {
+
+        return clawMotor.motor.getPosition();
+        
+    }
+
+    public void ClawSpinPositive(double speed) {
+
+        clawMotor.Spin(Math.abs(speed));
+
+    }
+
+    public void ClawSpinNegative(double speed) {
+
+        clawMotor.Spin(-Math.abs(speed));
+
+    }
+
+    public void ClawStop() {
+
+        clawMotor.Spin(0);
+
+    }
+}
