14434de7169a9ececc51fac0b30c63b6f2c2eff7
InfernoJager <eharrisonnc329@gmail.com>
2025-02-01 14:27:40 -0500

    LimelightSubsystem V1.0
    Started subsystem, much thinking needed about what this subsystem needs to do that limelight helpers doesn't have, if anything.

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.utils.LimelightHelpers;
+
+public class LimelightSubsystem extends SubsystemBase {
+
+    public LimelightSubsystem() {}
+    
+}
