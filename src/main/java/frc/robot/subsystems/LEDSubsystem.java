fce640af94a259e3469e16558c1373f4619d0ae1
InfernoJager <eharrisonnc329@gmail.com>
2025-02-01 14:15:53 -0500

    LEDSubsystem V1.1
    Updated LED Subsystem to be blinking and rio compatible

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,22 +1,61 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 
 public class LEDSubsystem extends SubsystemBase {
 
-    private final PWM led;
+    private PWM blinkinled;
+    private AddressableLED led;
+    private AddressableLEDBuffer ledBuffer;
 
     public LEDSubsystem() {
 
-        led = new PWM(0); // 0 is a placeholder
+        blinkinled = new PWM(0); // 0 is a placeholder
 
     }
 
-    public void SetLED(double speed) {
+    public void BlinkinSetLED(double speed) {
 
-        led.setSpeed(speed);
+        blinkinled.setSpeed(speed);
+
+    }
+
+    public LEDSubsystem(int ledLength) {
+
+        led = new AddressableLED(0); // 0 is a placeholder
+
+        ledBuffer = new AddressableLEDBuffer(ledLength);
+        led.setLength(ledBuffer.getLength());
+
+        led.setData(ledBuffer);
+        led.start();
+
+    }
+
+    public Color GetColor(int r, int g, int b) {
+
+        return new Color(r, g, b);
+
+    }
+
+    @Override
+    public void periodic() {
+
+        led.setData(ledBuffer);
+
+    }
+
+    public Command RunPattern(LEDPattern pattern) {
+
+        return run(() -> pattern.applyTo(ledBuffer));
 
     }
     
