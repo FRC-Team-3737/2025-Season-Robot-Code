4743b1d65565591484e128e2fec37a46ccc4a328
LyraLz <lyranielsen83@wcpsstudent.org>
2025-02-12 20:27:29 -0500

    Code V1.8.1
    Algae processor and place coral commands initial write.

---
+++ src/main/java/frc/robot/commands/ButtonCommands/AlgaeProcessorCommand.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.ButtonCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.utils.SubsystemList;
+import frc.robot.commands.ArmCommands.ArmPivotMoveCommand;
+import frc.robot.commands.ArmCommands.ArmPivotStopCommand;
+import frc.robot.commands.GrabberCommands.GrabberShootCommand;
+import frc.robot.commands.GrabberCommands.GrabberStopCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import frc.robot.subsystems.ArmSubsystem.armType;
+
+public class AlgaeProcessorCommand extends SequentialCommandGroup {
+
+    public AlgaeProcessorCommand(SubsystemList subsystems) {
+
+        addCommands(
+            new ArmPivotMoveCommand(subsystems, armType.grabber, 25, 3, 1, .5),
+            new GrabberShootCommand(subsystems, .5).alongWith(new ArmPivotStopCommand(subsystems, armType.grabber)).raceWith(new WaitCommand(2)),
+            new GrabberStopCommand(subsystems).alongWith(new ArmPivotMoveCommand(subsystems, armType.grabber, 10, 3, 0, .5)),
+            new ArmPivotStopCommand(subsystems, armType.grabber)
+        );
+
+    }
+
+}
---
+++ src/main/java/frc/robot/commands/ButtonCommands/PlaceCoralCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands.ButtonCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.utils.SubsystemList;
+import frc.robot.commands.ClawCommands.ClawOpenCommand;
+import frc.robot.commands.ClawCommands.ClawStopCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class PlaceCoralCommand extends SequentialCommandGroup {
+
+    public PlaceCoralCommand(SubsystemList subsystems) {
+
+        addCommands(
+            new ClawOpenCommand(subsystems, .5).raceWith(new WaitCommand(1)),
+            new ClawStopCommand(subsystems)
+        );
+
+    }
+
+}
