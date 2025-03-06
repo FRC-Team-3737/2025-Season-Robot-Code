88a050305ccd883ba904a93f6b93e094c33a073c
InfernoJager <eharrisonnc329@gmail.com>
2025-03-06 12:57:56 -0500

    Commands V1.3
    Updated if-else and else-if statements to switches in coral level command and algae intake command.

--- src/main/java/frc/robot/commands/ButtonCommands/AlgaeReefIntakeCommand.java
+++ src/main/java/frc/robot/commands/ButtonCommands/AlgaeReefIntakeCommand.java
@@ -3,41 +3,32 @@ package frc.robot.commands.ButtonCommands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.utils.SubsystemList;
-
-import frc.robot.commands.ArmCommands.ArmPivotMoveCommand;
-import frc.robot.commands.ArmCommands.ArmPivotStopCommand;
-
-import frc.robot.commands.GrabberCommands.AlgeaDetectionCommand;
-import frc.robot.commands.GrabberCommands.GrabberIntakeCommand;
-import frc.robot.commands.GrabberCommands.GrabberStopCommand;
+import frc.robot.commands.ArmCommands.ArmFullStopCommand;
+import frc.robot.commands.ArmCommands.ArmPivotCommand;
 
 import frc.robot.subsystems.ArmSubsystem.armType;
 
 public class AlgaeReefIntakeCommand extends SequentialCommandGroup {
 
-    public AlgaeReefIntakeCommand(SubsystemList subsystems, int level) {
+    public AlgaeReefIntakeCommand(SubsystemList subsystems, String algaeLevel) {
 
-        if (level == 1) {
+        switch (algaeLevel.toLowerCase()) {
 
-            addCommands(
-                
-                new ArmPivotMoveCommand(subsystems, armType.grabber, 45, 1, .5, .3),
-                new ArmPivotStopCommand(subsystems, armType.grabber),
-                new GrabberIntakeCommand(subsystems, .5).raceWith(new AlgeaDetectionCommand(subsystems)),
-                new GrabberStopCommand(subsystems).alongWith(new ArmPivotMoveCommand(subsystems, armType.grabber, 25, 1, 0, .3))
+            case "lower":
+                addCommands(
+                    new ArmPivotCommand(subsystems, armType.grabber, 0, 0)
+                );
 
-            );
+            case "upper":
+                addCommands(
+                    new ArmPivotCommand(subsystems, armType.grabber, 0, 0)
+                );
 
-        } else {
+            default:
+                addCommands(
+                    new ArmFullStopCommand(subsystems, armType.grabber)
+                );
 
-            addCommands(
-                
-                new ArmPivotMoveCommand(subsystems, armType.grabber, 90, 1, .5, .3),
-                new ArmPivotStopCommand(subsystems, armType.grabber),
-                new GrabberIntakeCommand(subsystems, .5).raceWith(new AlgeaDetectionCommand(subsystems)),
-                new GrabberStopCommand(subsystems).alongWith(new ArmPivotMoveCommand(subsystems, armType.grabber, 25, 1, 0, .3))
-
-            );
 
         }
 
--- src/main/java/frc/robot/commands/ButtonCommands/CoralLevelCommand.java
+++ src/main/java/frc/robot/commands/ButtonCommands/CoralLevelCommand.java
@@ -5,44 +5,49 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.utils.SubsystemList;
 
 import frc.robot.subsystems.ArmSubsystem.armType;
-import frc.robot.commands.ArmCommands.ArmPivotMoveCommand;
-import frc.robot.commands.ArmCommands.ArmPivotStopCommand;
 import frc.robot.commands.ArmCommands.ArmFullStopCommand;
+import frc.robot.commands.ArmCommands.ArmMoveCommand;
+import frc.robot.commands.ArmCommands.ArmPivotCommand;
 import frc.robot.commands.ClawCommands.WristPivotCommand;
-import frc.robot.commands.ClawCommands.WristStopCommand;
 
 public class CoralLevelCommand extends SequentialCommandGroup {
 
     public CoralLevelCommand(SubsystemList subsystems, int reefLevel) {
 
-        if (reefLevel == 1) {
+        switch (reefLevel) {
+            case 1:
+                addCommands(
+                    new ArmMoveCommand(subsystems, armType.claw, 0, 1.00),
+                    new ArmPivotCommand(subsystems, armType.claw, 0, 1.0).alongWith(
+                        new WristPivotCommand(subsystems, 90, 0.5))
+                );
 
-            addCommands(
-                new ArmPivotMoveCommand(subsystems, armType.claw, 45, 1, .1, .5),
-                new ArmPivotStopCommand(subsystems, armType.claw)
-            );
+            case 2:
+                addCommands(
+                    new ArmMoveCommand(subsystems, armType.claw, 0, 1.00),
+                    new ArmPivotCommand(subsystems, armType.claw, 0, 1.0).alongWith(
+                        new WristPivotCommand(subsystems, 90, 0.5))
+                );
 
-        } else if (reefLevel == 2) {
+            case 3:
+                addCommands(
+                    new ArmMoveCommand(subsystems, armType.claw, 0, 1.00),
+                    new ArmPivotCommand(subsystems, armType.claw, 0, 1.0).alongWith(
+                        new WristPivotCommand(subsystems, 90, 0.5))
+                );
 
-            addCommands(
-                new ArmPivotMoveCommand(subsystems, armType.claw, 90, 1, .1, .5),
-                new ArmPivotStopCommand(subsystems, armType.claw)
-            );
-
-        } else if (reefLevel == 3) {
-
-            addCommands(
-                new ArmPivotMoveCommand(subsystems, armType.claw, 125, 1, .1, .3),
-                new ArmPivotStopCommand(subsystems, armType.claw)
-            );
-
-        }  else {
-
-            addCommands(
-                new ArmPivotMoveCommand(subsystems, armType.claw, 150, 1, .1, .5),
-                new ArmPivotStopCommand(subsystems, armType.claw)
-            );
-
+            case 4:
+                addCommands(
+                    new ArmMoveCommand(subsystems, armType.claw, 0, 1.00),
+                    new ArmPivotCommand(subsystems, armType.claw, 0, 1.0).alongWith(
+                        new WristPivotCommand(subsystems, 90, 0.5))
+                );
+        
+            default:
+                addCommands(
+                    new ArmFullStopCommand(subsystems, armType.claw)  
+                );
+            
         }
 
     }
