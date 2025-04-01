14d5aee0e5f19e5b618a2df0e1a24f14f1bab5b7
InfernoJager <Eharrisonnc329@gmail.com>
2025-04-01 16:24:01 -0400

    Led V1.3.3
    Temporarily disabled animations.

--- src/main/java/frc/robot/beldorf.java
+++ src/main/java/frc/robot/beldorf.java
@@ -11,3 +11,4 @@ public class beldorf {
     }
     
 }
+/* Michael & Collin was here */
--- src/main/java/frc/robot/utils/LEDTriggerManager.java
+++ src/main/java/frc/robot/utils/LEDTriggerManager.java
@@ -36,9 +36,9 @@ public class LEDTriggerManager {
         defaultTrigger();
 
         // Match State
-        autoStartTrigger();
-        teleopStartTrigger();
-        endgameStartTrigger();
+        //autoStartTrigger();
+        //teleopStartTrigger();
+        //endgameStartTrigger();
 
         // Gameplay
         coralAcquiredTrigger();
